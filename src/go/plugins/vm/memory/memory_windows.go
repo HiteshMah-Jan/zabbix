ed153d271d2ca65bd50659e8572abb93122691e3
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-10-07 01:40:44 +0530

    Update memory_windows.go

--- src/go/plugins/vm/memory/memory_windows.go
+++ src/go/plugins/vm/memory/memory_windows.go
@@ -2,7 +2,7 @@
 ** Zabbix
 ** Copyright (C) 2001-2021 Zabbix SIA
 **
-** This program is free software; you can redistribute it and/or modify
+** This program is free software; you can redistribute it and/or modifycd
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
